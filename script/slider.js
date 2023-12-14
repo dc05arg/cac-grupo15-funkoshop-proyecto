137ee2385fd4489c1bb747bb5017310062c776de
maxiderecho <maxi.derecho@gmail.com>
2023-12-14 17:53:02 -0300

    Separo el código de JS en distintos archivos y los vinculo a los html. Corrijo width de los inputs para mobile.

--- script/slider.js
+++ script/slider.js
@@ -1,18 +1,3 @@
-//  Menú hamburguesa despegable en el navbar del header para versión mobile.
-
-const navbarIcon = document.querySelector(".navbar__icon");
-const navbarMenu = document.querySelector(".navbar__menu");
-
-navbarIcon.addEventListener("click", () => {
-  navbarMenu.classList.toggle("show-menu");
-});
-
-// Agregamos que ante cualquier scroll en la página el menú hamburguesa se oculte.
-
-document.addEventListener("scroll", () => {
-  navbarMenu.classList.remove("show-menu");
-});
-
 // Función para generar los funko-item
 const funkoData = [
   {
